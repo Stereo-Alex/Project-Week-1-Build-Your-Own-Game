1bbd2e999b0ec26673e86b340db1982eaf25aeb4
Stereo-Alex <alexjuradogoni@gmail.com>
2021-01-13 21:56:22 +0100

    issues with it breaking fixed, included a vs code option without arrays to try to turn it into user input

--- your-project/VS version of cpu guess the code .py
+++ your-project/VS version of cpu guess the code .py
@@ -1,22 +1,17 @@
 def cpu_function_range():
-    list_of_range = []
-    for number in range(1, 1001):
-        list_of_range.append(number)
-    return list_of_range
+    return list(range(1, 1001))
+    
 
 def cpu_game_choice():
-    num = int(input("Enter a number between 0 and 1000 for the computer to guess: "))
-    if num < 1:
-        print("Must be in range [1, 1000], please stick to the range im already having a hard enough time")
-        cpu_game_choice()
-        
-    elif num > 1000:
-        print("Must be in range [1, 1000], please stick to the range im already having a hard enough time")
-        cpu_game_choice()
-    
-    else:
-        print("Number picked, the computer will now try to guess", num)
-        return num
+    while True:
+        num = int(input("Enter a number between 0 and 1000 for the computer to guess: "))
+        if 1 > num or num > 1000:
+            print("Must be in range [1, 1000], please stick to the range im already having a hard enough time")
+            continue
+            
+        else:
+            print("Number picked, the computer will now try to guess", num)
+            return num
 
 
 def computer_guess(num, list_of_range):
