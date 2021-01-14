5f05e73cc6cd676ac1ad7b514b166a00cd5bf1c2
Stereo-Alex <alexjuradogoni@gmail.com>
2021-01-14 21:45:14 +0100

    final 2 working files and almost compleated read me, need to move stuff around and make pretty otherwise project completed

--- your-project/README.md
+++ your-project/README.md
@@ -1,9 +1,9 @@
 <img src="https://bit.ly/2VnXWr2" alt="Ironhack Logo" width="100"/>
 
 # Title of Your Project
-*[Your Name]*
+*Alex Jurado*
 
-*[Your Cohort, Campus & Date]*
+*[Data Analytics Jan 2020, Barcelona & 15 Jan 2020]*
 
 ## Content
 - [Project Description](#project-description)
@@ -13,21 +13,24 @@
 - [Links](#links)
 
 ## Project Description
-Write a short description of your project. Write 1-2 sentences about the game you chose to build and why.
+A game of guess the number with two options, one where the player can play a normal game of higher and lower and another one where the player can make the computer guess.
 
 ## Rules
-Briefly describe the rules of the game.
+Pick whether you want to play the game or you want to make the conmuper play. 
+Set a range for yours or the computers game. 
+If you have picked to play yourself thats it, have fun the computer will tell you how many tries you have taken.
+If you picked the computer to play, sit back and watch the computer take guesses.
 
 ## Workflow
-Outline the workflow you used in your project. What are the steps you went through?
+Started by creating the players game once it was working decided to try to make the computer playing too by using a binary search algorithm. After having both games operational, I decided to try slittign it into as many functions to keep it as "pure" as possible.
 
 ## Organization
-How did you organize your work? Did you use any tools like a kanban board?
+Started with using Trello and set some boundaries for my game, due to the rather simple nature of the game it was pretty free flowing afterwards. 
 
-What does your repository look like? Explain your folder and file structure.
+In one folder called old versions, I stored all the diferent stages the game went through, mostly the computer part of it. 
+In the main folder and .py folder is where the bulk of the code is stored and then a .ipynb document to run the game on.
 
 ## Links
-Include links to your repository, slides and kanban board. Feel free to include any other links associated with your project.
 
 [Repository](https://github.com/)  
 [Slides](https://slides.com/)  
---
+++ your-project/binary_ag.py
@@ -0,0 +1,87 @@
+def cpu_function_range():
+    low = int(input("Set the lower limit of the game, this gives the computer a chance to narrow it down"))
+    high = int(input("Set the higher limit of the game"))
+    return low, high
+
+
+def cpu_game_choice(low, high):
+    while True:
+        num = int(input(f"Enter a number between {low} and {high} for the computer to guess: "))
+        if low > num or num > high:
+            print(f"Must be in range [{low}, {high}], please stick to the range im already having a hard enough time")
+            continue
+
+        else:
+            print("Number picked, the computer will now try to guess", num)
+            return num
+
+
+def computer_guesses(num, low, high):
+    guesses = []
+    guess = None
+    while guess != num:
+        guess = (high - low) // 2 + low
+        guesses.append(guess)
+        if guess > num:
+            high = guess
+        elif guess < num:
+            low = guess + 1
+    return guesses
+
+
+def print_guesses(guesses):
+    for guess in guesses[:-1]:
+        print(f"The computer has guessed: {guess}")
+    print(f"The computer guessed {guesses[-1]} and it was correct!")
+
+
+
+
+
+
+def cpu_function_range_2(bound):
+    return int(input(f"Set the {bound} limit of the game"))
+
+
+
+def player_game(low, high):
+    import random
+    random_number = random.randrange(low, high)
+    print(random_number)
+    try_counter = 0
+    print(f"the game has started, your scoreboard is at {try_counter}")
+    player_guess = 0
+    while player_guess != random_number:
+        player_guess = int(input("Please enter your guess: "))
+        if player_guess < random_number:
+            print("Your guess is too low")
+            try_counter = try_counter + 1
+        if player_guess > random_number:
+            print("Your guess is too high")
+            try_counter = try_counter + 1
+    else:
+        print(f"you guessed right! The secret number was:  {random_number}")
+
+    final_result = print(f"you guessed the number in {try_counter}, turns")
+
+    return final_result
+
+
+
+
+def main():
+    game_choice = input("Wellcome to the game,"
+                        "\n"
+                        "do you want to play yourself or make the computer play?"
+                        "\n"
+                        "If you want to play yourself please input 1,"
+                        "\n"
+                        "if you would prefer the computer to play then press 2")
+
+    if game_choice == "1":
+        return player_game(cpu_function_range_2("lower"), cpu_function_range_2("upper"))
+
+    elif game_choice == "2":
+        low, high = cpu_function_range()
+        guesses = computer_guesses(cpu_game_choice(low, high), low, high)
+        return print_guesses(guesses)
